a1a6863a96250b936646474bbe57f46a2ec7c5ad
Joaquim <113483260+joaquim-de-melo-neto@users.noreply.github.com>
2024-09-13 17:31:01 -0300

    Update RepetitionInfoDemo.java

--- 08-Resolvers/RepetitionInfoDemo.java
+++ 08-Resolvers/RepetitionInfoDemo.java
@@ -2,11 +2,11 @@ class RepeatedTestsDemo {
   private Logger logger = // ...
   @BeforeEach
   void beforeEach(TestInfo testInfo, RepetitionInfo repetitionInfo) {
-  int currentRepetition = repetitionInfo.getCurrentRepetition();
-  int totalRepetitions = repetitionInfo.getTotalRepetitions();
-  String methodName = testInfo.getTestMethod().get().getName();
-  logger.info(String.format("About to execute repetition %d of %d for %s", //
-  currentRepetition, totalRepetitions, methodName));
+    int currentRepetition = repetitionInfo.getCurrentRepetition();
+    int totalRepetitions = repetitionInfo.getTotalRepetitions();
+    String methodName = testInfo.getTestMethod().get().getName();
+    logger.info(String.format("About to execute repetition %d of %d for %s", //
+                                currentRepetition, totalRepetitions, methodName));
   }
   @RepeatedTest(10)
   void repeatedTest() {
@@ -14,7 +14,7 @@ class RepeatedTestsDemo {
   }
   @RepeatedTest(5)
   void repeatedTestWithRepetitionInfo(RepetitionInfo repetitionInfo) {
-  assertEquals(5, repetitionInfo.getTotalRepetitions());
+    assertEquals(5, repetitionInfo.getTotalRepetitions());
   }
   @RepeatedTest(value = 1, name = "{displayName}
 {currentRepetition}/{totalRepetitions}")
