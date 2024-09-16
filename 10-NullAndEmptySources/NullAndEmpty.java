6e328df8970a9d8454e215f460d1506343c24130
Joaquim <113483260+joaquim-de-melo-neto@users.noreply.github.com>
2024-09-16 16:49:50 -0300

    Update NullAndEmpty.java

--- 10-NullAndEmptySources/NullAndEmpty.java
+++ 10-NullAndEmptySources/NullAndEmpty.java
@@ -12,3 +12,7 @@ void nullEmptyAndBlankStrings(String text) {
 void nullEmptyAndBlankStrings(String text) {
   assertTrue(text == null || text.trim().isEmpty());
 }
+
+/*Both variants of the nullEmptyAndBlankStrings(String) parameterized test method
+result in six invocations: 1 for null, 1 for the empty string, and 4 for the explicit
+blank strings supplied via @ValueSource.*/
