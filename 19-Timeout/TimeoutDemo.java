4dbf2e2c9956e7b6e18b5c7e340dda32409a3c58
joaquim <joaquim01001@gmail.com>
2024-09-26 16:45:03 -0300

    !9 timeout

---
+++ 19-Timeout/TimeoutDemo.java
@@ -0,0 +1,13 @@
+class TimeoutDemo {
+    @BeforeEach
+    @Timeout(5)
+    void setUp() {
+        // fails if execution time exceeds 5 seconds
+    }
+
+    @Test
+    @Timeout(value = 100, unit = TimeUnit.MILLISECONDS)
+    void failsIfExecutionTimeExceeds100Milliseconds() {
+        // fails if execution time exceeds 100 milliseconds
+    }
+}
