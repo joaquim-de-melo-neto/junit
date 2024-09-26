cae42223a4d13fb843aa8213122cc0c5c7a54f8f
joaquim <joaquim01001@gmail.com>
2024-09-26 16:39:37 -0300

    updated

--- 18-Dynamic_Test/DynamicTestsDemo.java
+++ 18-Dynamic_Test/DynamicTestsDemo.java
@@ -1,6 +1,5 @@
 import static example.util.StringUtils.isPalindrome;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -14,67 +13,67 @@ import java.util.Random;
 import java.util.function.Function;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
-import example.util.Calculator;
 import org.junit.jupiter.api.DynamicNode;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.api.function.ThrowingConsumer;
+
 class DynamicTestsDemo {
+
   private final Calculator calculator = new Calculator();
   // This will result in a JUnitException!
   @TestFactory
   List<String> dynamicTestsWithInvalidReturnType() {
-  return Arrays.asList("Hello");
+    return Arrays.asList("Hello");
   }
+
   @TestFactory
   Collection<DynamicTest> dynamicTestsFromCollection() {
-  return Arrays.asList(
-  dynamicTest("1st dynamic test", () -> assertTrue(isPalindrome("madam"))),
-  dynamicTest("2nd dynamic test", () -> assertEquals(4, calculator.multiply
-(2, 2)))
-  );
+    return Arrays.asList(
+    dynamicTest("1st dynamic test", () -> assertTrue(isPalindrome("madam"))),
+    dynamicTest("2nd dynamic test", () -> assertEquals(4, calculator.multiply(2, 2))));
   }
+
   @TestFactory
   Iterable<DynamicTest> dynamicTestsFromIterable() {
-  return Arrays.asList(
-  dynamicTest("3rd dynamic test", () -> assertTrue(isPalindrome("madam"))),
-  dynamicTest("4th dynamic test", () -> assertEquals(4, calculator.multiply
-(2, 2)))
-  );
+    return Arrays.asList(
+    dynamicTest("3rd dynamic test", () -> assertTrue(isPalindrome("madam"))),
+    dynamicTest("4th dynamic test", () -> assertEquals(4, calculator.multiply(2, 2))));
   }
 
   @TestFactory
   Iterator<DynamicTest> dynamicTestsFromIterator() {
-  return Arrays.asList(
-  dynamicTest("5th dynamic test", () -> assertTrue(isPalindrome("madam"))),
-  dynamicTest("6th dynamic test", () -> assertEquals(4, calculator.multiply
-(2, 2)))
-  ).iterator();
+    return Arrays.asList(
+    dynamicTest("5th dynamic test", () -> assertTrue(isPalindrome("madam"))),
+    dynamicTest("6th dynamic test", () -> assertEquals(4, calculator.multiply(2, 2))))
+    .iterator();
   }
+
   @TestFactory
   DynamicTest[] dynamicTestsFromArray() {
-  return new DynamicTest[] {
-  dynamicTest("7th dynamic test", () -> assertTrue(isPalindrome("madam"))),
-  dynamicTest("8th dynamic test", () -> assertEquals(4, calculator.multiply
-(2, 2)))
-  };
+    return new DynamicTest[] {
+    dynamicTest("7th dynamic test", () -> assertTrue(isPalindrome("madam"))),
+    dynamicTest("8th dynamic test", () -> assertEquals(4, calculator.multiply(2, 2)))};
   }
+
   @TestFactory
   Stream<DynamicTest> dynamicTestsFromStream() {
-  return Stream.of("racecar", "radar", "mom", "dad")
-  .map(text -> dynamicTest(text, () -> assertTrue(isPalindrome(text))));
+    return Stream.of("racecar", "radar", "mom", "dad")
+                 .map(text -> dynamicTest(text, () -> assertTrue(isPalindrome(text))));
   }
+
   @TestFactory
   Stream<DynamicTest> dynamicTestsFromIntStream() {
-  // Generates tests for the first 10 even integers.
-  return IntStream.iterate(0, n -> n + 2).limit(10)
-  .mapToObj(n -> dynamicTest("test" + n, () -> assertTrue(n % 2 == 0)));
+
+  //Generates tests for the first 10 even integers.
+    return IntStream.iterate(0, n -> n + 2).limit(10)
+    .mapToObj(n -> dynamicTest("test" + n, () -> assertTrue(n % 2 == 0)));
   }
+
   @TestFactory
   Stream<DynamicTest> generateRandomNumberOfTests() {
-  // Generates random positive integers between 0 and 100 until
-  // a number evenly divisible by 7 is encountered.
+  // Generates random positive integers between 0 and 100 until a number evenly divisible by 7 is encountered.
   Iterator<Integer> inputGenerator = new Iterator<Integer>() {
   Random random = new Random();
   int current;
@@ -97,27 +96,27 @@ class DynamicTestsDemo {
   // Returns a stream of dynamic tests.
   return DynamicTest.stream(inputGenerator, displayNameGenerator, testExecutor);
   }
+  
   @TestFactory
   Stream<DynamicNode> dynamicTestsWithContainers() {
-  return Stream.of("A", "B", "C")
-  .map(input -> dynamicContainer("Container " + input, Stream.of(
-  dynamicTest("not null", () -> assertNotNull(input)),
-  dynamicContainer("properties", Stream.of(
-  dynamicTest("length > 0", () -> assertTrue(input.length() > 0)),
-  dynamicTest("not empty", () -> assertFalse(input.isEmpty()))
-  ))
-  )));
+    return Stream.of("A", "B", "C")
+                 .map(input -> dynamicContainer("Container " + input, Stream.of(
+                                                            dynamicTest("not null", () -> assertNotNull(input)),
+                                                            dynamicContainer("properties", Stream.of(
+                                                            dynamicTest("length > 0", () -> assertTrue(input.length() > 0)),
+                                                            dynamicTest("not empty", () -> assertFalse(input.isEmpty()))
+    )))));
   }
+
   @TestFactory
   DynamicNode dynamicNodeSingleTest() {
-  return dynamicTest("'pop' is a palindrome", () -> assertTrue(isPalindrome("
-pop")));
+    return dynamicTest("'pop' is a palindrome", () -> assertTrue(isPalindrome("pop")));
   }
+
   @TestFactory
   DynamicNode dynamicNodeSingleContainer() {
-  return dynamicContainer("palindromes",
-  Stream.of("racecar", "radar", "mom", "dad")
-  .map(text -> dynamicTest(text, () -> assertTrue(isPalindrome(text)))
-  ));
+    return dynamicContainer("palindromes",
+                            Stream.of("racecar", "radar", "mom", "dad")
+                            .map(text -> dynamicTest(text, () -> assertTrue(isPalindrome(text)))));
   }
 }
